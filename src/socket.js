3bda6a3ac043f42135bf5fea431965ecbb06504d
JayanthReddy3010 <gjayanthreddy69@gmail.com>
2026-05-24 12:38:47 +0530

    fixed production socket

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
-});
+const socket = io(
+  "https://study-group-backend-b1kf.onrender.com",
+  {
+    transports: ["websocket", "polling"],
+  }
+);
 
 export default socket;
